8d6824bcbebcb8e20fd9fb55272ade1c84e4efcc
Rémi Cérès <remi.ceres@inria.fr>
2025-11-04 17:26:12 +0100

    refactor: remove unused SLF4J logger from QueryManager

--- src/main/java/fr/inria/corese/demo/manager/QueryManager.java
+++ src/main/java/fr/inria/corese/demo/manager/QueryManager.java
@@ -6,9 +6,6 @@ import fr.inria.corese.core.print.ResultFormat;
 import fr.inria.corese.core.query.QueryProcess;
 import fr.inria.corese.core.sparql.triple.parser.ASTQuery;
 import fr.inria.corese.core.sparql.triple.parser.Variable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,7 +22,6 @@ import java.util.Set;
  * -> getFormattedCachedQuery(tabId, format) -> clearCacheForTab(tabId).
  */
 public class QueryManager {
-  private static final Logger logger = LoggerFactory.getLogger(QueryManager.class);
   private static QueryManager instance;
 
   private final GraphManager graphManager;
@@ -213,7 +209,6 @@ public class QueryManager {
 
   public void addLogEntry(String entry) {
     logEntries.add(entry);
-    logger.debug("[QueryManager] {}", entry);
   }
 
   public List<String> getLogEntries() {
