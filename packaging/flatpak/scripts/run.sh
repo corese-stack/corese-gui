e35eb1b2cff7f5aa09578d0428491278613aa141
Rémi Cérès <remi.ceres@inria.fr>
2026-02-24 14:45:11 +0100

    flatpak: update runtime and adjust JavaFX permissions

--- packaging/flatpak/scripts/run.sh
+++ packaging/flatpak/scripts/run.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-exec /app/jre/bin/java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -jar /app/bin/corese-gui-standalone.jar "$@"
+# Prefer portals for open/save dialogs when available.
+export GTK_USE_PORTAL=1
+export GDK_BACKEND=x11
+
+exec /app/jre/bin/java \
+  --enable-native-access=ALL-UNNAMED \
+  -jar /app/bin/corese-gui-standalone.jar \
+  "$@"
